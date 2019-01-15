e3139441e52b5647dc83913a126e31e1eaab9b9e
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-15 15:51:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,9 @@
 def using_push(next_country,countries_in_western_africa)
-  @countries_in_western_africa.push(next_country)
+  countries_in_western_africa.push(next_country)
 end
 
 def using_unshift(neighborhoods_in_northwest_brooklyn,new_neighborhood)
-    @neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
 def using_pop(great_hits_of_the_nineties)
