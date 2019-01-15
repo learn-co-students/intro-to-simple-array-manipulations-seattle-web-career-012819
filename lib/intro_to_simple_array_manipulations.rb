d912fbc026e959ac4f7bcda2d21dbdedf85eb391
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-15 15:56:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,38 +11,38 @@ def using_pop(great_hits_of_the_nineties)
 end
 
 def pop_with_args(chars_in_game_of_thrones)
-  @chars_in_game_of_thrones.pop(2)
+  chars_in_game_of_thrones.pop(2)
 end
 
 def using_shift(my_favorite_cities)
-  @my_favorite_cities.shift 
+  my_favorite_cities.shift 
 end
 
 def shift_with_args(ice_cream_brands)
-  @ice_cream_brands.shift(2)
+  ice_cream_brands.shift(2)
 end
 
 def using_concat(my_favorite_things, more_favs)
-  @my_favorite_things.concat(more_favs)
+  my_favorite_things.concat(more_favs)
 end
 
 def using_insert(list_of_esoteric_programming_languages,another_esoteric_language)
-  @a_new_list = list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+  a_new_list = list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
 end
 
 def using_uniq(captain_planet_and_the_planeteers)
-  @new_array = captain_planet_and_the_planeteers.uniq 
+  new_array = captain_planet_and_the_planeteers.uniq 
   
 end
 
 def using_flatten(private_colleges_in_newyork)
-  @flat_array = private_colleges_in_newyork.flatten 
+  flat_array = private_colleges_in_newyork.flatten 
 end
 
 def using_delete(instructors, name =  "Steven")
-  @no_offense_steven = instructors.delete(name)
+  no_offense_steven = instructors.delete(name)
 end
 
 def using_delete_at(famous_robots, integer)
-  @deleted_robot = famous_robots.delete_at(integer)
+  deleted_robot = famous_robots.delete_at(integer)
 end
